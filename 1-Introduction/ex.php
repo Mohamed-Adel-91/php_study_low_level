057019c39c101de78ed776bbba0acdcdc28ec83f
MohamedAdel <mohamed101291@gmail.com>
2024-03-02 05:15:43 +0200

    lesson 1 -> Introduction

---
+++ 1-Introduction/ex.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <?php header('Content-Type: text/html; charset=utf-8');?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>
+    <?php echo "My first PHP page" ?>
+  </title>
+</head>
+
+<body>
+
+
+  <div>
+    <?php echo "We Love"; ?>
+  </div>
+
+</body>
+
+</html>
+
+<?php
+//echo 'Prevent Me From Running Please';
+#echo 'Prevent Me From Running Please';
+/* echo 'Prevent Me From Running Please'; */

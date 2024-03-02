24fbe7892658bd524d751a8603d4caa5bc19b29e
MohamedAdel <mohamed101291@gmail.com>
2024-03-02 05:51:41 +0200

    lesson 2 -> Data Types

---
+++ 2-Data Types/8-TypeCasting.php
@@ -0,0 +1,43 @@
+<?php
+
+  /*
+  ============================================
+  = Data Types
+  = ----------
+  = Type Casting
+  = ----------------------
+  = "boolean" or "bool"
+  = "integer" or "int"
+  = "float" or "double" or "real"
+  = "string"
+  = "array"
+  = "object"
+  = "null"
+  = ------
+  = Search For Settype
+  ============================================
+  */
+
+  echo 5 + (int) "5 Lessons"; //  Output: 10, because it converts the string to an integer before adding.
+  echo '<br>';
+  echo 5 + (integer) "5 Lessons"; //  Same as above; output: 10.
+  echo '<br>';
+  echo 5 + ( integer ) "5 Lessons";  // Same again!
+  echo '<br>';
+  echo gettype(5 + (int) "5 Lessons");    // Returns 'integer', indicating that the expression was evaluated as an integer.
+  echo '<br>';
+  echo 10 + 15.5; //25.5
+  echo '<br>';
+  echo 10 + (int) 15.5; //25
+  echo '<br>';
+  echo gettype(10 + (int) 15.5);  //Output: string, because PHP adds the number and then turns it into a string.
+  echo '<br>';
+  echo 10.5 + 10.5;  //21
+  echo '<br>';
+  echo gettype(10.5 + 10.5);  //Outputs: double
+  echo '<br>';
+  echo (int) 10.5 + (int) 10.5; // 20
+  echo '<br>';
+  echo gettype((int) 10.5 + (int) 10.5);    //Outputs: int
+  echo '<br>';
+  echo (int) (10.5 + 10.5); // 21
---
+++ 2-Data Types/9-BooleanAndConvertingToBoolean.php
@@ -0,0 +1,33 @@
+<?php
+
+  /*
+  ============================================
+  = Data Types
+  = ----------
+  = Boolean + Converting To Boolean
+  ============================================
+  */
+
+  var_dump((bool) ""); //  false
+  echo '<br>';
+  var_dump((bool) array());  // true
+  echo '<br>';
+  var_dump((bool) []);    // true
+  echo '<br>';
+  var_dump((bool) 0);      // false
+  echo '<br>';
+  var_dump((bool) "0");      // true
+  echo '<br>';
+  var_dump((bool) "Elzero");    // true
+  echo '<br>';
+  var_dump((bool) array(1));    // true
+  echo '<br>';
+  var_dump((bool) [1]);         // true
+  echo '<br>';
+  var_dump((bool) 100);         // true
+  echo '<br>';
+  var_dump((bool) -100);         // true
+  echo '<br>';
+  var_dump((bool) 10.5);         // true
+  echo '<br>';
+  var_dump((bool) -10.5);         // true

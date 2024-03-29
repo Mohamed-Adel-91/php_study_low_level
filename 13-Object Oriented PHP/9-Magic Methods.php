8857d6ae29fe2f5cd6c0f4ad0574033201b4cc61
MohamedAdel <mohamed101291@gmail.com>
2024-03-29 19:59:41 +0200

    oop

--- 13-Object Oriented PHP/9-Magic Methods.php
+++ 13-Object Oriented PHP/9-Magic Methods.php
@@ -29,8 +29,22 @@
 - __clone() is called whenever cloning occurs. It allows you to perform any clean up operation before cloning takes place.
 - __clone() is called when cloning an object. This can be useful if you want to perform some action whenever an object is copied.
 
+8.  Serialization & Unserialization -
+- serialize() converts an object into a string so that it can be stored in a file or database.
+- unserialize() does the reverse operation and rebuilds the original object from the serialized data.
+
+9. Dynamic methods - 
+- When PHP interpreter doesn’t find a method in the class it looks for dynamic methods.
+
+10. Call -
+- __call() Called when invoking function not accessible or not found
+- Accept 2 params [ $methodName , $params/$arguments ]
+- The call magic method allows objects to handle calls to methods 
+which do not exist in that particular object static methods using :: operator 
+
  */
 
+// __construct()
 class Dog 
 {
     public function __construct()
@@ -39,4 +53,31 @@ class Dog
     }
 }
 
-$animal = new Dog(); // Outputs: Dog barks with out call method
+$dog = new Dog(); // Outputs: Dog barks with out call method
+
+// __call()
+
+class Cat
+{
+  public $name;
+  public $color;
+  public function __call($methodName, $arguments)
+  {
+    if ($this->name == 'speak') 
+    {
+      echo "Meow! My name is " . $this->name . ". I am " . $this->color . "<br>";
+      print_r($arguments);
+    }
+    else  
+    {
+      die("Unable to execute method $methodName");
+    }
+  }
+}
+$cat = new Cat();
+$cat->name = "speak";
+$cat->color = "white";
+$cat->mow("cat",'dog','fox'); // Outputs: Meow! My name is speak . I am white
+echo '<pre>';
+print_r($cat);
+echo '</pre>';
